3481f1f642db0976dab168acc68037729e711f36
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-09-17 19:29:33 +0530

    Implement minSumOfLengths method in Solution class

--- Day4.java
+++ Day4.java
@@ -1,1 +1,34 @@
+class Solution {
+    public int minSumOfLengths(int[] arr, int target) {
+        int n = arr.length;
+        int[] minLen = new int[n];
+        
+        int left = 0;
+        int sum = 0;
+        int result = Integer.MAX_VALUE;
+        int minSoFar = Integer.MAX_VALUE;
 
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+
+            while (sum > target) {
+                sum -= arr[left];
+                left++;
+            }
+
+            if (sum == target) {
+                int currLen = right - left + 1;
+
+                if (left > 0 && minLen[left - 1] != Integer.MAX_VALUE) {
+                    result = Math.min(result, minLen[left - 1] + currLen);
+                }
+
+                minSoFar = Math.min(minSoFar, currLen);
+            }
+
+            minLen[right] = minSoFar;
+        }
+
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+}
