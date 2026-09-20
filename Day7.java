477050c2d2757200c9916e67114cee5937a18a0d
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-09-20 21:16:01 +0530

    Implement reverseDegree method in Solution class

--- Day7.java
+++ Day7.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int reverseDegree(String s) {
+        int totalSum = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int reversedAlphabetIndex = 26 - (c - 'a');
+            int stringIndex = i + 1;
+            
+            totalSum += reversedAlphabetIndex * stringIndex;
+        }
+        
+        return totalSum;
+    }
+}
