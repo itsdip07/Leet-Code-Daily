c4e3d923381fed04617af90153107b6fffb11280
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-09-21 19:26:45 +0530

    Implement resultArray method for subarray counts

--- Day8.java
+++ Day8.java
@@ -1,1 +1,31 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] result = new long[k];
+        long[] dp = new long[k];
 
+        for (int num : nums) {
+            int val = num % k;
+            long[] nextDp = new long[k];
+
+            // Subarray starting at current element
+            nextDp[val]++;
+
+            // Extend existing subarrays
+            for (int r = 0; r < k; r++) {
+                if (dp[r] > 0) {
+                    int newRem = (r * val) % k;
+                    nextDp[newRem] += dp[r];
+                }
+            }
+
+            // Accumulate counts into the final result
+            for (int r = 0; r < k; r++) {
+                result[r] += nextDp[r];
+            }
+
+            dp = nextDp;
+        }
+
+        return result;
+    }
+}
