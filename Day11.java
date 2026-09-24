53c169411c39e541c5541b63b0497b4737406058
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-09-24 19:24:14 +0530

    Add smallestIndex method to Solution class
    Implement method to find the smallest index where the sum of digits equals the index.

--- Day11.java
+++ Day11.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int smallestIndex(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            int temp = nums[i];
+            
+            // Calculate the sum of digits
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            
+            // If the sum equals the index, we return immediately 
+            // since we are iterating from the smallest index upwards.
+            if (sum == i) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
